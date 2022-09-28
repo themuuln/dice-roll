990783547bee3b32907c3eadc4123460ff249c01
Themuuln <zerone.offical@gmail.com>
2022-09-28 16:33:22 +0800

    tried to add several dice animation

--- script.js
+++ script.js
@@ -2,5 +2,22 @@ let btn = document.getElementById("mainBtn");
 let diceNumber = document.getElementById("dice");
 
 btn.addEventListener("click", () => {
-  diceNumber.innerText = Math.floor(Math.random() * 6 + 1);
+  randomNumberGenerator();
 });
+
+let randomNumberGenerator = () => {
+  diceNumber.innerText = Math.floor(Math.random() * 6 + 1);
+};
+
+let shakeEnabler = () => {
+  let pos = 0;
+  let id = setInterval(frame, 10);
+  let frame = () => {
+    if (pos == 50) {
+      clearInterval(id);
+    } else {
+      pos++;
+      btn.style.transform = ``
+    }
+  }
+};
