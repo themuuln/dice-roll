46c674d08f5be925f1565d29e38938bdf498c3d1
themuuln <zerone.offical@gmail.com>
2022-10-11 13:31:57 +0800

    added shake animation

--- script.js
+++ script.js
@@ -4,47 +4,57 @@ let total = document.querySelectorAll("#number");
 
 // Throwing a dice code
 
-let randomNumberGenerator = () => {
-  diceNumber.innerText = Math.floor(Math.random() * 6 + 1);
-};
+let numberGenerator = (_) =>
+  (diceNumber.innerHTML = Math.floor(Math.random() * 6 + 1));
 
 let storetotal = () => {
   total.innerText = "test";
 };
 
 btn.addEventListener("click", () => {
-  randomNumberGenerator();
+  numberGenerator();
   shakeEnabler();
+
+  // Button cooldown
+
+  setTimeout(() => {}, 1000);
 
   // Button on click animation
 
-  let clicked = false;
-  if (clicked == false) {
+  if (typeof NaN === "number") {
     btn.style.transform = "scale(1.2)";
     diceNumber.transform = "scale(1.1)";
-    clicked = true;
     setTimeout(() => {
       btn.style.transform = "scale(1)";
       diceNumber.transform = "scale(1)";
     }, 100);
   }
+
+  // Shake animation
+  diceNumber.style.animation = "shake .01s linear infinite";
+  setTimeout(() => {
+    diceNumber.style.animationPlayState = "paused";
+  }, 1000);
 });
 
-// Dice Animation
+// ! Bin
 
-let shakeEnabler = () => {
-  let timerId = null;
-  let trans = 0;
+// Dice Shaking Animation
+// Unnecessary for now
 
-  timerId = setInterval(frame, 0.1);
+// let shakeEnabler = () => {
+//   let timerId = null;
+//   let trans = 0;
 
-  function frame() {
-    if (trans == 360) {
-      clearInterval(timerId);
-    } else {
-      trans++;
-      // not working properly lol
-      // diceNumber.style.transform = "translate(" + trans + "px)";
-    }
-  }
-};
+//   timerId = setInterval(frame, 0.1);
+
+//   function frame() {
+//     if (trans == 360) {
+//       clearInterval(timerId);
+//     } else {
+//       trans++;
+// not working properly lol
+// diceNumber.style.transform = "translate(" + trans + "px)";
+//     }
+//   }
+// };
